9356ad13160046d13ca3804278b33ed3259fe5ee
neel-guckool <neel001g@gmail.com>
2026-06-24 16:34:36 +0100

    patterns

--- pyramid.py
+++ pyramid.py
@@ -1,7 +1,6 @@
-n = int(input("Enter how many rows you want for your pyramid: "))
+n = int(input("Enter how many rows you want for your half-pyramid: "))
 
 for i in range(n):
     for i in range(i+1):
         print("*" , end="")
     print()
-    
